6fcef803f50c3c60e80e6e5a2c949ea6cd5322a3
Seraj Eldeen <serageldeen13@gmail.com>
2025-11-18 20:22:06 +0200

    add:setup backend & databse

---
+++ backend/src/config/db.js
@@ -0,0 +1,19 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("❌ Database connection failed:", err);
+  } else {
+    console.log("✅ MySQL connected!");
+  }
+});
+export default db;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,32 @@
+import dotenv from "dotenv";
+dotenv.config();
 import cors from "cors";
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import AppError from "./services/appError.js";
+import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+import db from "./config/db.js";
 const app = express();
 
+app.use(helmet());
+app.use(morgan("common"));
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+app.use(limiter);
 app.use(express.json());
 app.use(cors());
-app.listen(5000, () => {
-  console.log(`App is running on port 5000`);
+
+app.use((req, res, next) =>
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+);
+
+app.use(globalErrorHandler);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
