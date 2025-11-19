fa2226ed125d8039cb05e4d50417f33f6aa188ed
Seraj Eldeen <serageldeen13@gmail.com>
2025-11-19 08:25:04 +0200

    add:setup Prisma and database tables

--- frontend/src/components/UnregisterHome.jsx
+++ frontend/src/components/UnregisterHome.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const UnregisterHome = () => {
+  const [isAvailable, setIsAvailable] = useState(false);
   return (
-    <div>
-      <p>UnregisterHome </p>
-    </div>
+    <section>
+      <div className="mx-5 my-10">
+        <button
+          className="bg-primary text-white rounded-md p-2 hover:bg-cta transition duration-300 ease-in-out"
+          onClick={() => setIsAvailable(!isAvailable)}
+        >
+          Click Me
+        </button>
+      </div>
+      {isAvailable ? (
+        <div className="text-red-500 m-5">Is Available</div>
+      ) : (
+        <div className="text-green-500 m-5">Is not Available</div>
+      )}
+    </section>
   );
 };
 
